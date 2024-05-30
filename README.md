08196584ccd5f62be3032bbf648a517dc901119f
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-05-30 11:31:05 -0300

    default-setup-app-2

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+Esta es la app por defecto para clonar y empezar a trabajar
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './css/main.css'
+
+function App() {
+
+  return (
+  <div className='chiche'>
+    <p>chiche</p>
+  </div>
+  )
+}
+
+export default App
